d87220569304178ff508a820c594e4d602591384
Suhas Bharadwaj <66212763+gaybowser69@users.noreply.github.com>
2022-03-13 14:23:18 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,9 +7,11 @@ void main()
     numNodes = 5;
     data = 0;
     weight = 1;
-
+    
+    /* Creating the graph */
     Graph* g = makeGraph(numNodes, data, weight);
 
+    /* Makes edges between all nodes in the graph */
     for (int i = 0; i < g->V; i++)
     {
         for (int j = 0; j < g->V; j++)
@@ -17,7 +19,12 @@ void main()
             makeEdge(g, i, j);
         }
     }
-
+    
+    /* Changing the data and weight of node 0 */
+    g->gMatrix[0][0]->data = 1;
+    g->gMatrix[0][0]->weight = 3;
+    
+    /* Destroys an edge between the specified indeces */
     destroyEdge(g, 0, 1);
 
     printGraph(g);
